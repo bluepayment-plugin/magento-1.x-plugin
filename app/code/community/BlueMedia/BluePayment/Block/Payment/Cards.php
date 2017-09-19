8d3512b4aff366d089ca6be6b90cf3b97d51f1d4
tk <tobiasz.kapusta@blueservices.pl>
2017-09-19 22:20:45 +0200

    more for one click

--- app/code/community/BlueMedia/BluePayment/Block/Payment/Cards.php
+++ app/code/community/BlueMedia/BluePayment/Block/Payment/Cards.php
@@ -1,17 +1,9 @@
 <?php   
-class BlueMedia_BluePayment_Block_Payment_Gateways extends Mage_Core_Block_Template{   
+class BlueMedia_BluePayment_Block_Payment_Cards extends Mage_Core_Block_Template{
 
 
-    protected $_gatewayList = array();
     protected $_cardsList = array();
 
-    public function getGatewaysList(){
-        if (!$this->_gatewayList){
-            $this->_gatewayList = Mage::getModel('bluepayment/bluegateways')->getCollection()
-                    ->addFieldToFilter('gateway_status', 1)->setOrder('gateway_sort_order', 'DESC');
-        }
-        return $this->_gatewayList;
-    }
 
     public function getUserCardList(){
         if (!$this->_cardsList){
--- app/code/community/BlueMedia/BluePayment/controllers/OnepageController.php
+++ app/code/community/BlueMedia/BluePayment/controllers/OnepageController.php
@@ -12,22 +12,20 @@ class BlueMedia_BluePayment_OnepageController extends Mage_Checkout_OnepageContr
     public function savePaymentAction() {
         $paymentMethod = $this->getRequest()->getPost('payment', array());
         if (isset($paymentMethod['method']) && $paymentMethod['method'] == 'bluepayment'){
-            $gatewayId = $this->getRequest()->getPost('payment_method_bluepayment_gateway');            
-            if ($gatewayId){
-                Mage::helper('bluepayment/gateways')->setQuoteGatewayId($gatewayId);
-                if ($gatewayId == Mage::getStoreConfig("payment/bluepayment/autopay_gateway")){
-                    $card_index = $this->getRequest()->getPost('payment_method_bluepayment_card_index');
-                    if ($card_index) {
-                        Mage::helper('bluepayment/gateways')->setQuoteCardIndex($card_index);
-                    } else {
-                        $result = array('error' => 'Nie wybrano karty lub opcji dodanie nowej karty!');
-                        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
-                        return;
-                    }
+            $gatewayId = $this->getRequest()->getPost('payment_method_bluepayment_gateway');
+            $card_index = $this->getRequest()->getPost('payment_method_bluepayment_card_index');
+
+            if ($gatewayId || $card_index){
+                if ($gatewayId){
+                    Mage::helper('bluepayment/gateways')->setQuoteGatewayId($gatewayId);
+                } else {
+                    Mage::helper('bluepayment/gateways')->setQuoteGatewayId(
+                        Mage::getStoreConfig("payment/bluepayment/autopay_gateway"));
+                    Mage::helper('bluepayment/gateways')->setQuoteCardIndex($card_index);
                 }
             }else{
                 if (Mage::helper('bluepayment/gateways')->isCheckoutGatewaysActive()){
-                    $result = array('error'=>'Nie wybrano kanału płatności!');
+                    $result = array('error'=>'Nie wybrano kanału płatności lub karty !');
                     $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
                     return;
                 }
